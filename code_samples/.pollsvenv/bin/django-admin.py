4bef3d026eacf4380e3baf45741609c7bdfa576f
Brian K. Vagnini <brian@vagnini.net>
2020-06-07 18:34:31 -0400

    Tighten up presentation. Run through Django tutorial.

---
+++ code_samples/.pollsvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/bkvagnini/projects/code/learning/bkvspeaks/pythonWebProg/code_samples/.pollsvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
